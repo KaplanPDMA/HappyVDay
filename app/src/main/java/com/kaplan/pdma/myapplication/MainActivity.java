76d5cbff9d2530366239aa90e85629c6f4b065d0
William Ku <kucheech@gmail.com>
2017-02-01 20:17:35 +0800

    Initial commit

---
+++ app/src/main/java/com/kaplan/pdma/myapplication/MainActivity.java
@@ -0,0 +1,26 @@
+package com.kaplan.pdma.myapplication;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final MediaPlayer mp = MediaPlayer.create(getApplicationContext(), R.raw.unbelievable);
+
+        Button b = (Button) findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mp.start();
+            }
+        });
+    }
+}
